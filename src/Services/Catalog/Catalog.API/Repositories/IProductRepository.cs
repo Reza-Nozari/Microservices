1e2c0be9b4e8140f621dfa2ef38fad548a051635
nozari <dev.nozari@gmail.com>
2022-04-13 17:01:11 +0430

    Implement Product And Context

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProductById(string id);
+        Task<IEnumerable<Product>> GetProductByName(string Name);
+        Task<IEnumerable<Product>> GetProductByCategory(string CategoryName);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+
+    }
+}
---
+++ src/Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task task = new Task(HelloWord);
+            task.Start();
+
+            Console.WriteLine("Main Task");
+
+        }
+
+        private static void HelloWord()
+        {
+            Console.WriteLine("Main Task");
+        }
+    }
+}
